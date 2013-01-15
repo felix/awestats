a588f0a9a4791a3f5433abf4400edcab6d61e8dc
Thomas PIERSON <thomas@mezcalito.fr>
2013-01-15 11:30:45 +0100

    add database sql schema

---
+++ documentation/create.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `email` varchar(250) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL COMMENT 'MD5',
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `websites` (
+    `url` varchar(250) NOT NULL,
+    `user_email` varchar(250) NOT NULL,
+    PRIMARY KEY (`url`),
+    FOREIGN KEY (user_email) REFERENCES users(email)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `session_id` varchar(250) NOT NULL,
+  `last_activity` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `data` text NOT NULL,
+  PRIMARY KEY (`session_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
