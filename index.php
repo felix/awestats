f12bcae8ac39da054af0eb0af6ca3a916da04408
Thomas PIERSON <thomas@mezcalito.fr>
2013-01-18 10:49:35 +0100

    reformat: netbeans auto

--- index.php
+++ index.php
@@ -8,57 +8,57 @@ require 'classes/User.php';
 use spriebsch\session\PHPMysqlSessionBackend;
 
 try {
-    
-$backend = new PHPMysqlSessionBackend(DB_USER, DB_PASSWORD, DB_NAME);
 
-$session = new Session($backend);
-$session->configure('session-awestats', BASE_URL);
-$session->start();
+    $backend = new PHPMysqlSessionBackend(DB_USER, DB_PASSWORD, DB_NAME);
 
-if(!empty($_POST['email']) && !empty($_POST['password'])) {
-    $email = (string)$_POST['email'];
-    $password = (string)$_POST['password'];
-    $user = new User($email, $password);
-    
-    if(!$user->isValid()) {
+    $session = new Session($backend);
+    $session->configure('session-awestats', BASE_URL);
+    $session->start();
+
+    if (!empty($_POST['email']) && !empty($_POST['password'])) {
+        $email = (string) $_POST['email'];
+        $password = (string) $_POST['password'];
+        $user = new User($email, $password);
+
+        if (!$user->isValid()) {
+            require 'controllers/render_login.php';
+            exit(0);
+        } else {
+            $session->setUser($user);
+            $session->commit();
+        }
+    }
+
+    if ($session->hasUser()) {
+        $user = $session->getUser();
+    } else {
         require 'controllers/render_login.php';
         exit(0);
-    } else {
-        $session->setUser($user);
-        $session->commit();
     }
-}
 
-if ($session->hasUser()) {
-    $user = $session->getUser();
-} else {
-    require 'controllers/render_login.php';
-    exit(0);
-}
-
-$sites = $user->getSites();
-$aConfig = array();
-foreach ($sites as $site) {
-    if(!empty($site)) {
-        $aConfig[$site] = array(
-            "statspath"   => "/var/lib/awstats/",
-            "statsname"   => "awstats[MM][YYYY].{$site}.txt",
-            "updatepath"  => "/usr/lib/cgi-bin/",
-            "siteurl"     => "http://{$site}/",
-            "sitename"    => $site,
-            "theme"       => "default",
-            "fadespeed"   => 250,
-            "password"    => "",
-            "includes"    => "",
-            "language"    => "en-gb"
-        );
+    $sites = $user->getSites();
+    $aConfig = array();
+    foreach ($sites as $site) {
+        if (!empty($site)) {
+            $aConfig[$site] = array(
+                "statspath" => "/var/lib/awstats/",
+                "statsname" => "awstats[MM][YYYY].{$site}.txt",
+                "updatepath" => "/usr/lib/cgi-bin/",
+                "siteurl" => "http://{$site}/",
+                "sitename" => $site,
+                "theme" => "default",
+                "fadespeed" => 250,
+                "password" => "",
+                "includes" => "",
+                "language" => "en-gb"
+            );
+        }
     }
-}
 
-if(empty($aConfig)) {
-    throw new Exception('Aucun sites associés à votre compte.');
-}
-
+    if (empty($aConfig)) {
+        throw new Exception('Aucun sites associés à votre compte.');
+    }
+    
 } catch (\Exception $e) {
     $error_message = $e->getMessage();
     require 'controllers/render_error.php';
@@ -67,24 +67,24 @@ if(empty($aConfig)) {
 
 // get ctr param
 $action = '';
-if(isset($_GET['action'])) {
-    $action = (string)$_GET['action'];
+if (isset($_GET['action'])) {
+    $action = (string) $_GET['action'];
 }
 // get year
 $now = new DateTime();
 $year = $now->format("Y");
-if(isset($_GET['year'])) {
-    $year = (string)$_GET["year"];
+if (isset($_GET['year'])) {
+    $year = (string) $_GET["year"];
 }
 // get month
 $month = $now->format("m");
-if(isset($_GET['month'])) {
-    $month = (string)$_GET["month"];
+if (isset($_GET['month'])) {
+    $month = (string) $_GET["month"];
 }
 // get view
 $view = CONFIG_DEFAULT_VIEW;
-if(isset($_GET['view'])) {
-    $view = (string)$_GET["view"];
+if (isset($_GET['view'])) {
+    $view = (string) $_GET["view"];
 }
 
 require_once "classes/clsAWStats.php";
